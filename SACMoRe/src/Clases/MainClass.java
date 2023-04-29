2656c97992c382a6081d718af67d909168e14be2
jwaibschnaider <jwaibschnaider@frba.utn.edu.ar>
2023-04-29 16:14:00 -0300

    entrega 1 ok

--- SACMoRe/src/Clases/MainClass.java
+++ SACMoRe/src/Clases/MainClass.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 public class MainClass {
 	public static void main(String[] args) {
 		Usuario usuario = new Usuario();
-		System.out.print("Ingrese una contra: ");
+		System.out.print("Ingrese una contrasenia: ");
 		Scanner s = new Scanner(System.in);
 		String contrasenia = s.nextLine();
 		
-		usuario.ValidarContrasenia(contrasenia);
+		usuario.setContrasenia(contrasenia);
+		s.close();
 	}
 }
--- SACMoRe/src/Clases/Usuario.java
+++ SACMoRe/src/Clases/Usuario.java
@@ -1,33 +1,19 @@
 package Clases;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-
 public class Usuario {
 	private String nombre;
 	private String apellido;
 	private String email;
-	private String contrasenia;
+	private Contrasenia contrasenia;
 
-	public Boolean ValidarContrasenia(String contraValidar) {
-		ArrayList<String> contraseniasDebiles = new ArrayList<String>();
-		String archivoContraseniasDebiles = "/src/Resources/peores_contrasenias.txt";
-		try {
-			contraseniasDebiles = (ArrayList<String>) Files.readAllLines(Paths.get(System.getProperty("user.dir")+ archivoContraseniasDebiles));
-		} catch (IOException e) {
-			System.out.println("No se pudo leer el archivo: " + e.getMessage());
-			return false;
-		}
+	public Contrasenia getContrasenia() {
+		return contrasenia;
+	}
 
-		if (contraseniasDebiles.contains(contraValidar)) {
-			System.out.println("Su contraseña es débil. Por favor, elija otra contraseña.");
-			return false;
-		} else {
-			System.out.println("Su contraseña es segura. ¡Buen trabajo!");
-			return true;
-		}
+	public void setContrasenia(String contrasenia) {
+		Contrasenia contra = new Contrasenia(contrasenia);
+		if (contra.ValidarContrasenia())
+			this.contrasenia = contra;
 	}
 
 }
