e314cb92f515531e70ccda814d36e8268497f6a5
jwaibschnaider <jwaibschnaider@frba.utn.edu.ar>
2023-08-03 18:52:48 -0300

    Saco el id secreto para que auth0 no llore

--- Entrega 3/src/Clases/Auth0/Auth0Config.java
+++ Entrega 3/src/Clases/Auth0/Auth0Config.java
@@ -1,7 +1,9 @@
 package Clases.Auth0;
 
+import Clases.Shared.ConfigReader;
+
 public class Auth0Config {
 	public static final String DOMAIN = "dev-pea42w5x2zewt3o8.us.auth0.com";
     public static final String CLIENT_ID = "pifhCZ5cyIAnpFfqCBA3x9lGsi2TfN2h";
-    public static final String CLIENT_SECRET = "oy2EYnkyRNdYXj-PBBRJKsO-TFSCfEdXdzk5-oenvyJg9B_1CCentFkrhq-gdGMZ";
+    public static final String CLIENT_SECRET = ConfigReader.getClientSecret();
 }
